2210538269fdcbed76e676562cbc597791ddfa9b
Shivang Singh <shivangsingh2240@gmail.com>
2022-09-25 05:20:09 +0530

    lol

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
 dotenv.config()
-const port = 3007 || process.env.PORT
+var port = 3007 || process.env.PORT
 // app.use(cors())   process.env.PORT
 
 const app = express()
